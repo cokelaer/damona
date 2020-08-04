fcc6c9b64cfb6b5e1a115dde7426dda2f1da797c
Thomas Cokelaer <cokelaer@gmail.com>
2020-08-04 13:33:33 +0200

    update test

--- test/test_damona.py
+++ test/test_damona.py
@@ -1,4 +1,4 @@
-from damona import damona
+from damona import script
 import subprocess
 
 
@@ -17,6 +17,26 @@ def test_pull():
     cmd = "damona list"
     subprocess.call(cmd.split())
 
+    try:
+        cmd = "damona develop"
+        subprocess.call(cmd.split())
+        assert False
+    except:
+        assert True
+
+    from damona.recipes import __path__
+    cmd = "damona develop --path {}".format(__path__[0] + "/fastq"  )
+    subprocess.call(cmd.split())
 
 
- 
+    cmd = "damona pull fastqc:0.11.9 --dryrun"  
+    subprocess.call(cmd.split())
+    
+
+    # non existing image
+    cmd = "damona pull tartuffe"  
+    try:
+        subprocess.call(cmd.split())    
+        assert False
+    except:
+        assert True
