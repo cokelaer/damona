638442c18fe1cf9c81360305ebd3233a4d2518a8
Thomas Cokelaer <cokelaer@gmail.com>
2021-03-05 21:48:32 +0100

    fix test

--- test/test_registry.py
+++ test/test_registry.py
@@ -1,4 +1,4 @@
-from damona.registry import SingleRegistry, Registry
+from damona.registry import Software, Registry
 import glob
 
 
@@ -13,19 +13,19 @@ def test_singe_registry():
 
     # binaries is not provided and found from the registry name
     salmon_registry = [x for x in _registry_files if "salmon" in x][0]
-    sr = SingleRegistry(salmon_registry)
+    sr = Software(salmon_registry)
     assert sr.data['releases']['1.3.0']['binaries'] == ['salmon']
 
 
     # binaries is provided explicitly in the general section
     fastqc_registry = [x for x in _registry_files if "/fastqc/" in x][0]
-    sr = SingleRegistry(fastqc_registry)
+    sr = Software(fastqc_registry)
     assert sr.data['releases']['0.11.9']['binaries'] == ['fastqc']
 
     # binaries are provided explicitly in the releases section. Here there are
     # two releases with different binary names
     kraken_registry = [x for x in _registry_files if "kraken" in x][0]
-    sr = SingleRegistry(kraken_registry)
+    sr = Software(kraken_registry)
     assert sr.data['releases']['1.1']['binaries'] == ['kraken']
     assert sr.data['releases']['2.0.9']['binaries'] == ['kraken2', 'kraken2-build']
 
