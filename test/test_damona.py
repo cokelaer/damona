55b5e676ff132ee0f2a6951e6ae733add717ab2c
Thomas Cokelaer <cokelaer@gmail.com>
2021-03-05 18:00:37 +0100

    better tests

--- test/test_damona.py
+++ test/test_damona.py
@@ -1,7 +1,8 @@
 from damona import script
-from damona import pull
+import damona
 import subprocess
 import builtins
+
 
 def test_damona_app():
     from click.testing import CliRunner
@@ -10,17 +11,13 @@ def test_damona_app():
     runner = CliRunner()
 
     # isntall
-    results = runner.invoke(install, ['fastqc:0.11.9', '--dryrun'])
+    results = runner.invoke(install, ['fastqc:0.11.9'])
     assert results.exit_code == 0
 
     results = runner.invoke(_list)
     assert results.exit_code == 0
 
-    import damona
-    results = runner.invoke(registry, ["--path", damona.__path__[0] + "/recipes/fastqc"])
-    assert results.exit_code == 0
     
-    import damona
     results = runner.invoke(env, [])
     # wrong
     results = runner.invoke(env, ["--create", ".dummy_test", "--delete", ".dummy_test"])
@@ -39,44 +36,6 @@ def test_damona_app():
         results = runner.invoke(env, ["--delete", ".dummy_test"])
         assert results.exit_code == 0
 
-def test_environ():
-    from damona.environ import Environ
-    env = Environ()
-    env.N
-    env.environments
-    env.create(".dummy_test")
-    import mock
-    with mock.patch.object(builtins, 'input', lambda _: 'y'):
-        env.delete(".dummy_test")
-
-def test_config():
-    from damona.config import Config
-    c = Config()
-    c.read()
-
-
-def test_python_pull():
-    p = pull.Pull(dryrun=True)
-    p.pull("fastqc:0.11.9")
-    p.pull("fastqc") # latest
-    try:
-        p.pull("fastqc1")
-        assert False
-    except:
-        assert True
-
-def test_python_registry():
-    from damona import registry
-    r = registry.Registry()
-    r.get_list()
-
-    from damona.recipes import __path__
-    path = __path__[0] + "/fastqc"
-    r.create_registry(path)
-
-    r = registry.Registry("damona")
-    r.get_list()
-
 
 def test_help():
     cmd = "damona --help"
@@ -92,25 +51,14 @@ def test_list():
     cmd = "damona list --pattern qc"
     subprocess.call(cmd.split())
 
-def test_registry():
-    try:
-        cmd = "damona registry"
-        subprocess.call(cmd.split())
-        assert False
-    except:
-        assert True
 
-    from damona.recipes import __path__
-    cmd = "damona registry --path {}".format(__path__[0] + "/fastq"  )
+def test_install_dryrun():
+    cmd = "damona install fastqc:0.11.9 --dryrun"
     subprocess.call(cmd.split())
 
-def test_pull_dryrun():
-    cmd = "damona pull fastqc:0.11.9 --dryrun"  
-    subprocess.call(cmd.split())
-
-def test_pull_wrong():
+def test_install_wrong():
     # non existing image
-    cmd = "damona pull tartuffe"  
+    cmd = "damona install tartuffe"  
     try:
         subprocess.call(cmd.split())    
         assert False
